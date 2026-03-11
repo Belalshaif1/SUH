c5e55efa0cc90afa1b35adb86aaac6c19e3b73d8
Belalshaif1 <bilalshaif216@gmail.com>
2026-03-11 23:10:27 +0300

    Update server.js
    Signed-off-by: Belalshaif1 <bilalshaif216@gmail.com>

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.use('/api/sync', syncRoutes); // ربط مسارات المزامنة
 
 
 // نقطة فحص سريعة للتأكد من أن السيرفر يعمل بشكل سليم
-app.get('/api/health', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ status: 'ok', message: 'Server is running', timestamp: new Date() });
 });
 
